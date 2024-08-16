a605ce04f387c7f31d71afd642c543b58a01f24a
Trushar Radadiya <82151539+TrusharRadadiya@users.noreply.github.com>
2024-08-16 17:33:26 +0530

    refactor: Use reflection to set google ads app IDs

--- _Scripts/GoogleMobileAds/Editor/GoogleMobileAdsIDSetter.cs
+++ _Scripts/GoogleMobileAds/Editor/GoogleMobileAdsIDSetter.cs
@@ -1,23 +1,34 @@
+using System;
+using System.Reflection;
 using MyAdvertisement.Utilities.Editor;
 using UnityEditor;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
+using Object = UnityEngine.Object;
 
 namespace MyAdvertisement.Editor
 {
     public class GoogleMobileAdsIDSetter: IPreprocessBuildWithReport
     {
-        public int callbackOrder => 0;
+        public int callbackOrder => -1;
         
         public void OnPreprocessBuild(BuildReport report)
         {
-            // GoogleMobileAdNetwork googleMobileAdNetwork = ScriptableObjectUtility.FindAsset<GoogleMobileAdNetwork>(); 
-            // GoogleMobileAdsSettings googleMobileAdsSettings = GoogleMobileAdsSettings.LoadInstance();
-            // string androidAppID = googleMobileAdNetwork.Testing ? googleMobileAdNetwork.TestAppID : googleMobileAdNetwork.AndroidAppID;
-            // string iOSAppID = googleMobileAdNetwork.Testing ? googleMobileAdNetwork.TestAppID : googleMobileAdNetwork.IOSAppID;
-            // googleMobileAdsSettings.GoogleMobileAdsAndroidAppId = androidAppID;
-            // googleMobileAdsSettings.GoogleMobileAdsIOSAppId = iOSAppID;
-            // EditorUtility.SetDirty(googleMobileAdsSettings);
+            GoogleMobileAdNetwork googleMobileAdNetwork = ScriptableObjectUtility.FindAsset<GoogleMobileAdNetwork>();
+            string androidAppID = googleMobileAdNetwork.Testing ? googleMobileAdNetwork.TestAppID : googleMobileAdNetwork.AndroidAppID;
+            string iOSAppID = googleMobileAdNetwork.Testing ? googleMobileAdNetwork.TestAppID : googleMobileAdNetwork.IOSAppID;
+            
+            Assembly assembly = Assembly.Load("GoogleMobileAds.Editor");
+            Type type = assembly.GetType("GoogleMobileAds.Editor.GoogleMobileAdsSettings");
+            MethodInfo loadInstanceMethodInfo = type.GetMethod("LoadInstance", BindingFlags.NonPublic | BindingFlags.Static);
+            object obj = loadInstanceMethodInfo.Invoke(null, null);
+
+            PropertyInfo androidAdIdInfo = type.GetProperty("GoogleMobileAdsAndroidAppId");
+            PropertyInfo iOSAdIdInfo = type.GetProperty("GoogleMobileAdsIOSAppId");
+            
+            androidAdIdInfo.SetValue(obj, androidAppID);
+            iOSAdIdInfo.SetValue(obj, iOSAppID);
+            EditorUtility.SetDirty(obj as Object);
         }
     }
 }
